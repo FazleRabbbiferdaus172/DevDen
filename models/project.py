0144c998a33c82b068276bb8421c338fcfe5b100
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-10-26 00:17:46 +0600

    project table updated

--- models/project.py
+++ models/project.py
@@ -1,7 +1,19 @@
 from .base_model import BaseModel
+from .db import db
 
 class Project(BaseModel):
+    id:int
     name:str
     des:str
     link:str
     type:str
+    profile_id:int
+
+
+class ProjectImage(BaseModel):
+    id: int
+    project_id: int
+    image_data: bytes
+
+projects = db.create(Project)
+project_images = db.create(ProjectImage)
