b09991add13c16a24e56520e769093c678912821
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-11-06 02:30:22 +0600

    table edit post route is added

--- routes/admin.py
+++ routes/admin.py
@@ -42,9 +42,14 @@ def table_record_create_form(table_name: str, id: int):
     form_view = template_form_view(table_name=table_name, mode='edit', record_id=id)
     return main_template(form_view)
 
-# @app.post("/admin/{table_name}/new")
 @admin_routes(path="/admin/{table_name}/new", methods=['post'])
 def table_record_create_form(table_name: str, data: dict):
     table = get_table_by_name(table_name)
     table.insert(data)
+    return RedirectResponse(f'/admin/{table_name}', status_code=303)
+
+@admin_routes(path="/admin/{table_name}/{id}", methods=['post'])
+def table_record_create_form(table_name: str, data: dict, id: int):
+    table = get_table_by_name(table_name)
+    table.update(updates=data, pk_values=id)
     return RedirectResponse(f'/admin/{table_name}', status_code=303)
