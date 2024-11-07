236586f2d09f66e103c3e1c4b9a67aa16d41995c
Fazle Rabbi Ferdas <fazle.ferdaus1416@gmail.com>
2024-11-08 03:22:56 +0600

    public tamplates are broken down, now it is time to make them dynamic

--- templates/main_public_template.py
+++ templates/main_public_template.py
@@ -1,46 +1,89 @@
 from fasthtml.components import *
-
-def generate_how_to_reach_public():
-        return [Span("Contact Me -", cls='title is-capitalized is-5'),
-                        Span(Span("fazle.ferdaus1416@gmail.com "), Span(I(cls="fas fa-envelope")), Span(" -"), cls='custom-block has-text-weight-semibold'),
-                        Span(Span("+880 1968628234 "), Span(I(cls="fas fa-phone")), Span(" -"),cls='custom-block has-text-weight-semibold'),
-                        Span(Span("House#34, Road#7, Block#E, Mirpur-12, Dhaka, Bangladesh "), Span(I(cls="fas fa-map-pin")), Span(" -"), cls='custom-block has-text-weight-semibold')]
+from templates.infinite_scroll_view import generate_infnite_scroll_list_public
 
 def generate_about_section_public():
     return [Span("About Me -", cls='title is-capitalized is-5'),
-                        Span("From Dhaka, Bangladesh -", cls='custom-block has-text-weight-semibold'),
-                        Span("Currently working at Brain Station 23 Plc. -", cls='custom-block has-text-weight-semibold'),
-                        Span("Enjoys building things and swimming -", cls='custom-block has-text-weight-semibold')]
+            Span("From Dhaka, Bangladesh -",
+                 cls='custom-block has-text-weight-semibold'),
+            Span("Currently working at Brain Station 23 Plc. -",
+                 cls='custom-block has-text-weight-semibold'),
+            Span("Enjoys building things and swimming -", cls='custom-block has-text-weight-semibold')]
 
-def main_public_template(*args, **kwargs):
-    icon_list = Div(
-        A(I(cls="fab fa-github fa-2x block link"), cls="link", href="https://github.com/FazleRabbbiferdaus172"),
-        A(I(cls="fab fa-linkedin-in fa-2x block link"),  cls="link", href="https://www.linkedin.com/in/fazle-rabbi-ferdaus-113255185/"),
-        A(I(cls="fab fa-facebook fa-2x block link"), cls="link", href="https://www.facebook.com/FazleRabbiFerdaus/"),
-        cls="public-links"
-    )
+def generate_project_public(table_name='project', part_num=0):
+    return generate_infnite_scroll_list_public('project', part_num)
+
+def generate_how_to_reach_public():
+    return [Span("Contact Me -", cls='title is-capitalized is-5'),
+            Span(Span("fazle.ferdaus1416@gmail.com "), Span(I(cls="fas fa-envelope")),
+                 Span(" -"), cls='custom-block has-text-weight-semibold'),
+            Span(Span("+880 1968628234 "), Span(I(cls="fas fa-phone")),
+                 Span(" -"), cls='custom-block has-text-weight-semibold'),
+            Span(Span("House#34, Road#7, Block#E, Mirpur-12, Dhaka, Bangladesh "), Span(I(cls="fas fa-map-pin")), Span(" -"), cls='custom-block has-text-weight-semibold')]
+
+def generate_header_title():
+    title = H1('Fazle Rabbi Ferdaus', cls='title is-1')     
+    return title
+
+def generate_header_subtitle():
+    subtitle = H2('Software Enginner', cls='subtitle is-4')
+    return subtitle
+
+def generate_header_nav():
     nav = Nav(
-        A(Span('Home', cls='is-size-5 has-text-left'), cls="side-nav selected", hx_get=f'about/page',
-          hx_trigger='click',hx_target='#main-content-right',hx_swap='innerHtml'),
-        Span(Span('Projects', cls='is-size-5 has-text-left'), cls="side-nav", hx_get=f'project/page?idx={1}',
-          hx_trigger='click',hx_target='#main-content-right',hx_swap='innerHtml'),
-        A(Span('How to reach me', cls='is-size-5 has-text-left'),  cls="side-nav", hx_get=f'contact/page',
-          hx_trigger='click',hx_target='#main-content-right',hx_swap='innerHtml'),
+        A(Span('Home', cls='is-size-5 has-text-left'), cls="side-nav selected", hx_get=f'about/page/',
+          hx_trigger='click', hx_target='#main-content-right', hx_swap='innerHtml'),
+        Span(Span('Projects', cls='is-size-5 has-text-left'), cls="side-nav", hx_get=f'project/page/?idx={1}',
+             hx_trigger='click', hx_target='#main-content-right', hx_swap='innerHtml'),
+        A(Span('How to reach me', cls='is-size-5 has-text-left'),  cls="side-nav", hx_get=f'contact/page/',
+          hx_trigger='click', hx_target='#main-content-right', hx_swap='innerHtml'),
         cls="block pt-5 pl-5 public-nav",
     )
-    header = Header(H1('Fazle Rabbi Ferdaus',cls='title is-1'), H2('Software Enginner', cls='subtitle is-4'), nav, cls='block public-header public-section-left')
-    about_me_section = generate_about_section_public()
-    # projects = generate_infnite_scroll_list_public('project')
+    return nav
+
+def generate_public_header():
+    header = Header(
+        generate_header_subtitle(), 
+        generate_header_title(), 
+        generate_header_nav(), 
+        cls='block public-header public-section-left')
+    return header
+
+def generate_default_public_main_content():
+    return generate_about_section_public()
+
+def generate_public_main():
+    default_main_content = generate_default_public_main_content()
     main = Main(
-                Div(
-                    Div(
-                        *about_me_section,
-                        # *projects,
-                        id="main-content-right"
-                    ),
-                cls='block content is-normal is-pullued-bottom-right public-section-right')
-                )
-    footer = Footer(icon_list, cls="public-footer")
-    body = (Title("Ferdaus's Den"),Body(Div(header, main, cls="public"), footer)
+        Div(
+            Div(
+                *default_main_content,
+                id="main-content-right"
+            ),
+            cls='block content is-normal is-pullued-bottom-right public-section-right')
     )
+    return main
+
+def generate_public_footer_content():
+    icon_list = Div(
+        A(I(cls="fab fa-github fa-2x block link"), cls="link",
+          href="https://github.com/FazleRabbbiferdaus172"),
+        A(I(cls="fab fa-linkedin-in fa-2x block link"),  cls="link",
+          href="https://www.linkedin.com/in/fazle-rabbi-ferdaus-113255185/"),
+        A(I(cls="fab fa-facebook fa-2x block link"), cls="link",
+          href="https://www.facebook.com/FazleRabbiFerdaus/"),
+        cls="public-links"
+    )
+    return icon_list
+
+def generate_public_footer():
+    footer_content = generate_public_footer_content()
+    footer = Footer(footer_content, cls="public-footer")
+    return footer
+
+def main_public_template(*args, **kwargs):
+    header = generate_public_header()
+    main = generate_public_main()
+    footer = generate_public_footer()
+    body = (Title("Ferdaus's Den"), Body(Div(header, main, cls="public"), footer)
+            )
     return body
