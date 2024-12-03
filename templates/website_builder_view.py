b803a8e68bfa9c41299f5f7227a5488d82a4e59a
Fazle Rabbi Ferdaus <fazle.ferdaus1416@gmail.com>
2024-12-03 20:54:28 +0600

    grid layout in website builder

--- templates/website_builder_view.py
+++ templates/website_builder_view.py
@@ -10,4 +10,6 @@ def website_builder_section():
     pass
 
 def website_builder_view():
-    return Div("Hello from website builder view")
+    div_list = [Div(f"{i}", cls='cell has-background-primary',  style="height: 100%;") for i in range(12)]
+    grid_div = Div(*div_list, cls='grid', style="height: 100%;")
+    return Div(grid_div, cls="fixed-grid has-3-cols", style="height: 100%;")
